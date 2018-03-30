2abcdd6f5c0e0f539df5d95245497e7be3926c76
Sami Hamdan <samihamdan00@gmail.com>
2018-03-30 20:10:44 +0000

    improved guess function

--- src/assets/main.js
+++ src/assets/main.js
@@ -3,11 +3,15 @@ let attempt = document.getElementById('attempt');
 
 function guess() {
     let input = document.getElementById('user-guess');
+    if (answer == '' && attempt == 0){
+        setHiddenFields();
+    }
     //add functionality to guess function here
 }
 
 //implement new functions here
 function setHiddenFields(){
+    attempt = 0;
     answer = Math.floor(Math.random()*10000 + 1).toString();
     while (answer.length < 4){
         answer = "0" + answer;
